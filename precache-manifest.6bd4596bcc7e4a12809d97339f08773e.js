481aa2ba8d732652fe554f30b5b725e0a18db867
SimonLiu <milkjhang@icloud.com>
2020-09-28 16:42:15 +0800

    first commit

---
+++ precache-manifest.6bd4596bcc7e4a12809d97339f08773e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4c800fdd1244f62e1eca136b9f86f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67da0742f87a6f1b0d2d",
+    "url": "/static/css/main.5b8532c6.chunk.css"
+  },
+  {
+    "revision": "cc760b9a984c01c2aab2",
+    "url": "/static/js/2.31f3b855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31f3b855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67da0742f87a6f1b0d2d",
+    "url": "/static/js/main.3484f13e.chunk.js"
+  },
+  {
+    "revision": "d5910de8f0c639286dc1",
+    "url": "/static/js/runtime-main.49c54174.js"
+  }
+]);
